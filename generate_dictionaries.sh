b1693ec2dc4e9b23a6c532b6734c37d917b83c4d
simon628 <faradaycurieplanck@gmail.com>
2017-12-29 22:39:45 -0300

    Gxisdatigi lingvnomoj.
    englic = englix
    do = to

--- generate_dictionaries.sh
+++ generate_dictionaries.sh
@@ -1,4 +1,4 @@
-# ze englic do pandunia
+# ze englix to pandunia
 cat english/pandunia-english.md | sed '/^#/ d' > temp/temp2.txt
 cat temp/temp2.txt temp/alfahead.txt > temp/temp.txt
 sed 's/\[.*\]/:/g' temp/temp.txt > temp/temp2.txt 
@@ -11,19 +11,19 @@ cp english/pandunia-english.md temp/temp.txt
 sed 's/_//g' -i temp/temp.txt
 cat temp/tiddly_1.html temp/temp.txt temp/tiddly_3.html > english/tiddly.html
 
-# ze suomi do pandunia
+# ze suomi to pandunia
 cat suomi/pandunia-suomi.md | sed '/^#/ d' > temp/temp2.txt
 cat temp/temp2.txt temp/alfahead.txt > temp/temp.txt
 awk ' { FS="\t"; OFS=" : "; t = $1; $1 = $2; $2 = t; print; } ' temp/temp.txt | sed 's/^ : //g' | LC_ALL=C sort -f | sed 's/.00/##/g' > suomi/suomi-pandunia.md
 sed 's/_//g' -i suomi/suomi-pandunia.md
 sed 's/$/  /' -i suomi/suomi-pandunia.md
 
-# ze pandunia do ruski
+# ze pandunia to ruski
 cat russki/slovar.txt temp/alfahead.txt > temp/temp.txt
 sed 's/	/ : /g' temp/temp.txt | LC_ALL=C sort -f | sed 's/.00/##/g' > russki/pandunia-ruski.md
 sed 's/_//g' -i russki/pandunia-ruski.md
 sed 's/$/  /' -i russki/pandunia-ruski.md
-# ze ruski do pandunia
+# ze ruski to pandunia
 #cat russki/slovar.txt temp/kirilitsa.txt > temp/temp.txt
 #awk ' { FS="\t"; OFS=" : "; t = $1; $1 = $2; $2 = t; print; } ' temp/temp.txt | sed 's/^ : //g' | LC_COLLATE=ru_RU.UTF-8 sort --ignore-case | sed 's/.00/##/g' > russki/ruski-pandunia.md
 cp russki/slovar.txt temp/temp.txt
@@ -31,7 +31,7 @@ awk ' { FS="\t"; OFS=" : "; t = $1; $1 = $2; $2 = t; print; } ' temp/temp.txt | 
 sed 's/_//g' -i russki/ruski-pandunia.md
 sed 's/$/  /' -i russki/ruski-pandunia.md
 
-# ze esperanto do pandunia
+# ze esperanto to pandunia
 cat esperanto/pandunia-esperanto.md | sed '/^#/ d' > temp/temp2.txt
 cat temp/temp2.txt temp/alfahead.txt > temp/temp.txt
 awk ' { FS=" : "; OFS=" : "; t = $1; $1 = $2; $2 = t; print; } ' temp/temp.txt | sed 's/^ : //g' | LC_ALL=C sort -f | sed 's/.00/##/g' > esperanto/esperanto-pandunia.md
